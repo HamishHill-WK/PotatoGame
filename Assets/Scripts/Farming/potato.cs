c6b6333a4b40b34a8c762bdbfeb1035aa5ebb1fe
Hamish <hamishhill1909@gmail.com>
2022-04-26 16:12:41 +0100

    expertiments

--- Assets/Scripts/Farming/potato.cs
+++ Assets/Scripts/Farming/potato.cs
@@ -11,22 +11,47 @@ public class potato : MonoBehaviour
 
     public int stock = 0;
 
-    public Text txt;
+    public Text txt;    
+    public Text nameTxt;
+
+    private string varName = "potato";
+
+    public float diseaseResist;
+    public float moistureReq;
+    public int[] plantingMonths;
 
     public void addStock(int add)
     {
         stock += add;
+
+        if (stock >= 99)
+            stock = 99;
+
+        txt.text = stock.ToString();
+
+    }
+
+    public void setStock(int add)
+    {
+        stock = add;
+        txt.text = stock.ToString();
+    }
+
+    public int getStock()
+    {
+        return stock;
+    }
+
+    public void setName(string newName)
+    {
+        varName = newName;
+        this.gameObject.name = varName;
     }
 
     void Start()
     {
-        PlayerData data = SaveSystem.LoadPlayer();
-
-        stock = data.totalPotatos;
-    }
-
-    void Update()
-    {
         txt.text = stock.ToString();
+       // nameTxt.text = varName;
     }
+
 }
---
+++ Assets/Scripts/Farming/potatoTypes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//this script was written by Hamish Hill GitHub: @HamishHill-WK
+//this script assigns names to potato objects from the "potatoNames.txt" file
+
+public class potatoTypes : MonoBehaviour
+{
+    public TextAsset nameFile;
+    public string[] fileNames = null;
+
+    void Start()
+    {
+        fileNames = nameFile.text.Split('\n');
+
+        int i = 0;
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<potato>().setName(fileNames[i]);
+            i++;
+        }
+    }
+}
